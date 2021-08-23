61f42bfde8aae71eaf257a0f72bdd6866b3817ed
larena1 <60823161+larena1@users.noreply.github.com>
2021-08-23 15:56:48 +0200

    Update MockLocationProvider.java

--- app/src/main/java/cl/coders/faketraveler/MockLocationProvider.java
+++ app/src/main/java/cl/coders/faketraveler/MockLocationProvider.java
@@ -65,6 +65,7 @@ public class MockLocationProvider {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
             mockLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
         }
+        mockLocation.setPowerRequirement(2);
         lm.setTestProviderLocation(providerName, mockLocation);
     }
 
